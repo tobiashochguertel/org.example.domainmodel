8a2dc83c3c98ed8d97ed71fbcc81235f5c22e099
Tobias Hochguertel <tobias.hochguertel@googlemail.com>
2023-12-24 21:40:50 +0100

    Done Extended: Creating Custom Validation Rules

--- org.example.domainmodel/src/org/example/domainmodel/validation/DomainmodelValidator.java
+++ org.example.domainmodel/src/org/example/domainmodel/validation/DomainmodelValidator.java
@@ -3,23 +3,43 @@
  */
 package org.example.domainmodel.validation;
 
+import org.eclipse.xtext.validation.Check;
+import org.eclipse.xtext.validation.CheckType;
+import org.example.domainmodel.domainmodel.DomainmodelPackage;
+import org.example.domainmodel.domainmodel.Entity;
+import org.example.domainmodel.domainmodel.Feature;
+
+import com.google.common.base.Objects;
 
 /**
- * This class contains custom validation rules. 
+ * This class contains custom validation rules.
  *
- * See https://www.eclipse.org/Xtext/documentation/303_runtime_concepts.html#validation
+ * See
+ * https://www.eclipse.org/Xtext/documentation/303_runtime_concepts.html#validation
  */
 public class DomainmodelValidator extends AbstractDomainmodelValidator {
-	
-//	public static final String INVALID_NAME = "invalidName";
-//
-//	@Check
-//	public void checkGreetingStartsWithCapital(Greeting greeting) {
-//		if (!Character.isUpperCase(greeting.getName().charAt(0))) {
-//			warning("Name should start with a capital",
-//					DomainmodelPackage.Literals.GREETING__NAME,
-//					INVALID_NAME);
-//		}
-//	}
-	
+
+	public static final String INVALID_NAME = "invalidName";
+
+	@Check
+	public void checkNameStartsWithCapital(Entity entity) {
+		if (!Character.isUpperCase(entity.getName().charAt(0))) {
+			warning("Name should start with a capital", DomainmodelPackage.Literals.TYPE__NAME, INVALID_NAME);
+		}
+	}
+
+	@Check(CheckType.NORMAL)
+	public void checkFeatureNameIsUnique(Feature feature) {
+		Entity superEntity = ((Entity) feature.eContainer()).getSuperType();
+		while (superEntity != null) {
+			for (Feature other : superEntity.getFeatures()) {
+				if (Objects.equal(feature.getName(), other.getName())) {
+					error("Feature names have to be unique", DomainmodelPackage.Literals.FEATURE__NAME);
+					return;
+				}
+			}
+			superEntity = superEntity.getSuperType();
+		}
+	}
+
 }
